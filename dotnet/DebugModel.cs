eb076bdf5e2c01a9e4f84cf7daa697d1eabf7bb7
Justin113D <Justin113D@gmail.com>
2023-11-22 18:38:59 +0100

    Added DebugModel class back in

--- dotnet/DebugModel.cs
+++ dotnet/DebugModel.cs
@@ -1,7 +1,7 @@
-﻿/* Unavailable until SA3D.Modeling.JSON is available
-     
-using SA3D.Modeling.ModelData;
+﻿using SA3D.Modeling.JSON;
+using SA3D.Modeling.Mesh;
 using SA3D.Modeling.ObjectData;
+using System.IO;
 using System.Text.Json;
 
 namespace SAIO.NET
@@ -13,27 +13,35 @@ namespace SAIO.NET
         public AttachFormat Format { get; set; }
         public bool Optimize { get; set; }
         public bool IgnoreWeights { get; set; }
-        public bool IgnoreRoot { get; set; }
         public bool WriteSpecular { get; set; }
         public bool AutoNodeAttributes { get; set; }
 
-        public DebugModel(NodeStruct[] nodes, MeshStruct[] weightedAttaches, AttachFormat format, bool optimize, bool ignoreWeights, bool ignoreRoot, bool writeSpecular, bool autoNodeAttributes)
+        public DebugModel(NodeStruct[] nodes, MeshStruct[] weightedAttaches, AttachFormat format, bool optimize, bool ignoreWeights, bool writeSpecular, bool autoNodeAttributes)
         {
             Nodes = nodes;
             WeightedAttaches = weightedAttaches;
             Format = format;
             Optimize = optimize;
             IgnoreWeights = ignoreWeights;
-            IgnoreRoot = ignoreRoot;
             WriteSpecular = writeSpecular;
             AutoNodeAttributes = autoNodeAttributes;
         }
 
         public void ToFile(string filename)
-            => File.WriteAllText(filename, JsonSerializer.Serialize(this, Structs.Json.JsonConverters.GetOptions()));
+        {
+            JsonSerializerOptions options = JsonConverters.GetOptions();
+            options.ReferenceHandler = new JsonReferenceHandler();
+
+            File.WriteAllText(filename, JsonSerializer.Serialize(this, options));
+        }
 
         public static DebugModel FromFile(string filename)
-            => JsonSerializer.Deserialize<DebugModel>(File.ReadAllText(filename), Structs.Json.JsonConverters.GetOptions());
+        {
+            JsonSerializerOptions options = JsonConverters.GetOptions();
+            options.ReferenceHandler = new JsonReferenceHandler();
+
+            return JsonSerializer.Deserialize<DebugModel>(File.ReadAllText(filename), options);
+        }
 
         public Node ToNodeStructure()
         {
@@ -43,11 +51,8 @@ namespace SAIO.NET
                 Format,
                 Optimize,
                 IgnoreWeights,
-                IgnoreRoot,
                 WriteSpecular,
                 AutoNodeAttributes);
         }
     }
 }
-
-*/
