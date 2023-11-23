bc9420cc5f3a4fcad9ccc075b477cd62b3ba419f
Justin113D <Justin113D@gmail.com>
2023-11-24 00:45:29 +0100

    added dev init file for test builds

--- build_dev.bat
+++ build_dev.bat
@@ -9,7 +9,7 @@ mkdir ".\build\SonicAdventureBlenderIO_dev"
 mkdir ".\build\SonicAdventureBlenderIO_dev\DLL"
 mkdir ".\build\SonicAdventureBlenderIO_dev\source"
 
-copy ".\blender\__init__.py" ".\build\SonicAdventureBlenderIO_dev\__init__.py"
+copy ".\blender\__initdev__.py" ".\build\SonicAdventureBlenderIO_dev\__init__.py"
 copy ".\blender\SAIOTemplates.blend" ".\build\SonicAdventureBlenderIO_dev\SAIOTemplates.blend"
 copy ".\blender\DLL" ".\build\SonicAdventureBlenderIO_dev\DLL"
 robocopy ".\blender\source" ".\build\SonicAdventureBlenderIO_dev\source" /s /xd __pycache__
