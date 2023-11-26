0e5f28864871cca973718a79f1dc95eb8914f54f
Justin113D <Justin113D@gmail.com>
2023-11-26 18:25:38 +0100

    Added addon header script for incorrect installations

---
+++ __init__.py
@@ -0,0 +1,22 @@
+bl_info = {
+    "name": "Sonic Adventure I/O !! INCORRECTLY INSTALLED !!",
+    "author": "Justin113D, ItsEasyActually, X-Hax",
+    "description": "INCORRECTLY INSTALLED THE ADDON! Make sure to download from the github repositories release section!",
+    "version": (0, 0, 0),
+    "blender": (4, 0, 0),
+    "location": "",
+    "warning": "",
+    "doc_url": "https://x-hax.github.io/SonicAdventureBlenderIODocs/",
+    "tracker_url": "https://github.com/X-Hax/SonicAdventureBlenderIO/issues/new",
+    "category": "Import-Export"
+}
+
+def register():
+    raise RuntimeError((
+        "\n\n!! Incorrectly installed the addon!!"
+
+        "\n\nMake sure to download SAIO from the github repositories release section:"
+        "\nhttps://github.com/X-Hax/SonicAdventureBlenderIO/releases/"
+
+        "\n\n\"You fucked up bud\""
+        "\n    - ItsEasyActually"))
