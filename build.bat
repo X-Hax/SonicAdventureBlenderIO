08f121176481a0ea64aefedcb617aa9460aa661c
Justin113D <Justin113D@gmail.com>
2024-07-28 22:49:44 +0200

    Updated build scripts and manifest

--- build.bat
+++ build.bat
@@ -1,19 +1,9 @@
 @echo off
 
 if exist ".\build" (
-	rmdir "./build" /s /q
+	rmdir ".\build" /s /q
 )
 
-mkdir ".\build"
-mkdir ".\build\SonicAdventureBlenderIO"
-mkdir ".\build\SonicAdventureBlenderIO\DLL"
-mkdir ".\build\SonicAdventureBlenderIO\source"
-
-copy ".\blender\__init__.py" ".\build\SonicAdventureBlenderIO\__init__.py"
-copy ".\blender\SAIOTemplates.blend" ".\build\SonicAdventureBlenderIO\SAIOTemplates.blend"
-copy ".\blender\DLL" ".\build\SonicAdventureBlenderIO\DLL"
-robocopy ".\blender\source" ".\build\SonicAdventureBlenderIO\source" /s /xd __pycache__
-
-pushd ".\build"
-tar -acf SonicAdventureBlenderIO.zip SonicAdventureBlenderIO
-popd
+mkdir .\build
+blender --command extension build --source-dir .\blender --output-dir .\build
+blender --command extension server-generate --repo-dir=.\build --html
--- build_dev.bat
+++ build_dev.bat
@@ -1,19 +1,15 @@
 @echo off
 
+move .\blender\blender_manifest.toml .\blender\blender_manifest.prod.toml
+move .\blender\blender_manifest.dev.toml .\blender\blender_manifest.toml
+
 if exist ".\build" (
-	rmdir "./build" /s /q
+	rmdir ".\build" /s /q
 )
 
-mkdir ".\build"
-mkdir ".\build\SonicAdventureBlenderIO_dev"
-mkdir ".\build\SonicAdventureBlenderIO_dev\DLL"
-mkdir ".\build\SonicAdventureBlenderIO_dev\source"
+mkdir .\build
+blender --command extension build --source-dir .\blender --output-dir .\build
+blender --command extension server-generate --repo-dir=.\build --html
 
-copy ".\blender\__initdev__.py" ".\build\SonicAdventureBlenderIO_dev\__init__.py"
-copy ".\blender\SAIOTemplates.blend" ".\build\SonicAdventureBlenderIO_dev\SAIOTemplates.blend"
-copy ".\blender\DLL" ".\build\SonicAdventureBlenderIO_dev\DLL"
-robocopy ".\blender\source" ".\build\SonicAdventureBlenderIO_dev\source" /s /xd __pycache__
-
-pushd ".\build"
-tar -acf SonicAdventureBlenderIO_dev.zip SonicAdventureBlenderIO_dev
-popd
+move .\blender\blender_manifest.toml .\blender\blender_manifest.dev.toml
+move .\blender\blender_manifest.prod.toml .\blender\blender_manifest.toml
