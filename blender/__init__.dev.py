addeb8ebfb442c488a90463d53dfa0979200aeec
Justin113D <Justin113D@gmail.com>
2024-08-06 13:19:00 +0200

    Updated dev build number

--- blender/__init__.dev.py
+++ blender/__init__.dev.py
@@ -10,7 +10,7 @@ bl_info = {
     "name": "Sonic Adventure I/O DEV BUILD",
     "author": "Justin113D, ItsEasyActually, X-Hax",
     "description": "Import/Exporter for Sonic Adventure Model, Animation and other Formats.",
-    "version": (2, 200, 5),
+    "version": (2, 200, 6),
     "blender": (4, 2, 0),
     "location": "",
     "warning": "",
