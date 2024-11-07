ec7c92c55c4acd1ad84ee931807b2640589dff1b
Justin113D <Justin113D@gmail.com>
2024-11-07 18:34:05 +0100

    Removed unnecessary wheels

--- blender/wheels/download_wheels.bat
+++ blender/wheels/download_wheels.bat
@@ -15,21 +15,11 @@ IF EXIST %py% (
 	%download%macosx_11_0_arm64
 
 	%download%musllinux_1_1_aarch64
-	%download%musllinux_1_1_i686
-	%download%musllinux_1_1_x86_64
-
-	%download%manylinux_2_12_i686
-	%download%manylinux_2_17_i686
 	%download%manylinux_2_17_aarch64
-	%download%manylinux_2_17_ppc64le
-	%download%manylinux_2_17_s390x
-	%download%manylinux_2_17_x86_64
-
-	%download%manylinux2010_i686
-	%download%manylinux2014_i686
 	%download%manylinux2014_aarch64
-	%download%manylinux2014_ppc64le
-	%download%manylinux2014_s390x
+
+	%download%musllinux_1_1_x86_64
+	%download%manylinux_2_17_x86_64
 	%download%manylinux2014_x86_64
 
 ) ELSE (
