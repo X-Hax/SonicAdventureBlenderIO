b9aaa8b67b7d5709cce381d6180023be068d9911
Justin113D <Justin113D@gmail.com>
2025-01-05 21:25:24 +0100

    updated wheels

--- blender/__init__.dev.py
+++ blender/__init__.dev.py
@@ -10,7 +10,7 @@ bl_info = {
     "name": "Sonic Adventure I/O DEV BUILD",
     "author": "Justin113D, ItsEasyActually, X-Hax",
     "description": "Import/Exporter for Sonic Adventure Model, Animation and other Formats.",
-    "version": (2, 201, 4),
+    "version": (2, 201, 5),
     "blender": (4, 2, 3),
     "location": "",
     "warning": "",
